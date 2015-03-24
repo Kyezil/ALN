15907ca57fb38ff88136ef066fa831497a8444c2
Louis Clergue <louisclergue@gmail.com>
2015-03-24 23:01:31 +0100

    Change Matrix to squared matrix for op optimization

---
+++ classes/SQ_Matrix.cc
@@ -0,0 +1,43 @@
+#include "SQ_Matrix.hh"
+
+SQ_Matrix::SQ_Matrix(const unsigned n) : N(n), rows(rows_){
+    mat.reserve(N);
+    rows_.reserve(N);
+    for (int i = 0; i < N; ++i) {
+        mat[i].reserve(N);
+        rows_[i] = i;
+    }
+}
+SQ_Matrix::~SQ_Matrix() {}
+
+void SQ_Matrix::swap_row(const unsigned i1, const unsigned i2) {
+    std::swap(rows_[i1], rows_[i2]);
+    std::swap(mat[i1], mat[i2]);
+    std::clog << "Swapped rows " << i1 << " and " << i2 << std::endl;
+}
+
+double& SQ_Matrix::operator()(const unsigned i, const unsigned j) {
+    return mat[i][j];
+}
+double SQ_Matrix::operator()(const unsigned i, const unsigned j) const {
+    return mat[i][j];
+}
+
+void SQ_Matrix::read() {
+    for (int i = 0; i < N; ++i)
+        for (int j = 0; j < N; ++j)
+            std::cin >> mat[i][j];
+    std::clog << "Have read the matrix!" << std::endl;
+}
+
+void SQ_Matrix::print(std::string name = "SQ_Matrix") const {
+    std::cout.precision(OUT_DIGITS);
+    std::cout.setf(std::ios::fixed);
+    std::cout << name << std::endl;
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            std::cout << std::right << std::setw(10) << mat[i][j];
+        }
+        std::cout << std::endl;
+    }
+}
---
+++ classes/SQ_Matrix.hh
@@ -0,0 +1,44 @@
+/* Class SQ_Matrix */
+#ifndef SQ_MATRIX_HH
+#define SQ_MATRIX_HH
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+#include <vector>
+class SQ_Matrix {
+    typedef std::vector<double> VD;
+    typedef std::vector<VD> VVD;
+    typedef std::vector<int> VI;
+    private:
+        VVD mat;
+    protected:
+        VI rows_;
+        const static unsigned OUT_DIGITS = 5;
+    public:
+        const unsigned N;
+        const VI& rows;;
+
+        SQ_Matrix(const unsigned n);
+        //\pre  true
+        //\post creates a matrix NxN
+
+        ~SQ_Matrix();
+
+        double& operator()(const unsigned i, const unsigned j);
+        double operator()(const unsigned i, const unsigned j) const;
+        // define operator for access
+
+        void swap_row(const unsigned i1, const unsigned i2);
+        //\pre  0 <= i1, i2 < N  i1 != i2
+        //\post permut row i1 <-> row i2
+        //\cost constant
+
+        void read();
+        //\pre  initialized MxN matrix and MxN doubles at input
+        //\post read doubles
+        
+        void print(std::string name) const;
+        //\pre true
+        //\post print content of matrix with name as header
+};
+#endif
