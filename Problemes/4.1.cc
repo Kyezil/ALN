fc5a921d74d525af7ca8be1e3ae2d39d271c1cf0
Louis Clergue <louisclergue@gmail.com>
2015-05-09 14:29:28 +0200

    exercici 4.1

--- Problemes/4.1.cc
+++ Problemes/4.1.cc
@@ -2,18 +2,43 @@
 #include <cmath>
 using namespace std;
 
-double i_exp_mcL(double x, double coef, int i, int N) {
+template<typename T>
+T i_exp_mcL(T x, T coef, int i, int N) {
     // coef = 1/i!  0 <= i <= N
     if (i == N) return coef;
-    else return coef + x*i_exp_mcL(x, coef/(i+1), i+1, N);
+    return coef + x*i_exp_mcL(x, coef/(i+1), i+1, N);
 }
 
-double exp_mcL(double x, int N) {
+template<typename T>
+T exp_mcL(T x, int N) {
     // PRE N >= 0
     // POST e^x usant Horner amb polinomi de McLaurin de grau N
-    return i_exp_mcL(x, 1, 0, N);
+    return i_exp_mcL<T>(x, 1, 0, N);
+}
+
+template<typename T>
+void taula(int x) {
+    T expC = exp(x);
+    printf("exp(%d)\t%f\n", x, expC);
+    T expMC = exp_mcL<T>(x,10);
+    T err = fabs(expMC-expC);
+    printf("p10(%d)\t%f\t%f\t%f\n", x, expMC, err, err/fabs(expMC));
+
+    expMC = 1/exp_mcL<T>(-x,10);
+    err = fabs(expMC-expC);
+    printf("q10(%d)\t%f\t%f\t%f\n", x, expMC, err, err/fabs(expMC));
+}
+
+void mostra_taules(int x) {
+    cout << "Precisió simple\n";
+    taula<float>(x);
+    cout << "\nPrecisió doble\n";
+    taula<double>(x);
+    cout << '\n';
 }
 
 int main() {
-    cout << exp_mcL(0.5,10) << endl;
+    // aproximació de e^(-2)
+    mostra_taules(-2);
+    mostra_taules(-7);
 }
