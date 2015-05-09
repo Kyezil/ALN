278513be2ca66e177c2e5c4ae94ce9421fabc650
Louis Clergue <louisclergue@gmail.com>
2015-05-09 19:00:00 +0200

    comment header

--- Programes/C++/vector.hh
+++ Programes/C++/vector.hh
@@ -6,24 +6,38 @@ class Vector {
         double * m_v;
         int m_size;
     public:
-        Vector(int size);
+        // Crea un vector de mida c_size
+        Vector(int c_size);
+        // Crea una copia de other
         Vector(Vector const &other);
+        // Crea per desplaçament
         Vector(Vector &&other);
+        // Destructor
         ~Vector();
 
+        // Assignació
         Vector& operator=(Vector const &other);
+        // Desplaçament
         Vector& operator=(Vector const &&other);
+        // Suma convencional
         Vector& operator+(Vector const &other);
+        // Resta convencional
         Vector& operator-(Vector const &other);
+        // Producte escalar de vectors
         double operator*(Vector const &other);
 
+        // Accés element rvalue
         double operator()(int i) const;
+        // Accés element lvalue
         double& operator()(int i);
 
-        inline void swap(int i, int j);
+        // Permuta 2 elements
+        void swap(int i, int j);
 
+        // Consulta la mida
         int size() const;
 
+        // Càlcul de normes convencionals
         static double norm1(const Vector &v);
         static double norm2(const Vector &v);
         static double normInf(const Vector &v);
