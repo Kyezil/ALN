72211842999181dee21b3cfcd0b30ef352fef4e8
Louis Clergue <louisclergue@gmail.com>
2015-05-10 16:13:31 +0200

    resolució AX=B per gauss sense pivot

---
+++ Programes/Octave/gauss_elim.m
@@ -0,0 +1,17 @@
+function [x] = gauss_elim(A, B)
+    % Es pot eliminació gaussiana sense pivotatge:
+    %   <=> els determinants principals són != 0
+    % Per resoldre AX = B
+    [n,n] = size(A);
+    [n,k] = size(B);
+    x = zeros(n,k);
+    for i=1:n-1
+        if (A(i,i) == 0) error('Pivot nul!'); end;
+        m = -A(i+1:n,i)/A(i,i);
+        A(i+1:n,:) = A(i+1:n,:) + m*A(i,:);
+        B(i+1:n,:) = B(i+1:n,:) + m*B(i,:);
+    endfor
+
+    % suBstitució enrere
+    x = back_substitution(A,B);
+endfunction
