c39616f010de93089079ffdfa52f952708b02e82
Louis Clergue <louisclergue@gmail.com>
2015-05-14 17:49:24 +0200

    + jacobi amb B i rho

--- Programes/Octave/jacobi.m
+++ Programes/Octave/jacobi.m
@@ -1,3 +1,4 @@
+% PER COMPROVAR EL RADI ESPECTRAL rho = max(abs(eig(B)));
 function [x dx k] = jacobi(A,b,x0,tol,maxit)
     % Mètode iteratiu de Jacobi per resoldre Ax=b
     % [IN] A matriu amb diagonal != 0    nxn
@@ -8,12 +9,38 @@ function [x dx k] = jacobi(A,b,x0,tol,maxit)
     % [OUT] x solució del sistema        nx1
     % [OUT] dx mesura de l'error         real
     % [OUT] k iteració on s'ha parat  enter
+    % condició radi espectral de (D\(A-D)) < 1
     n = length(b);
     x = x0;
     for k = 1:maxit
         for r = 1:n
             x(r) = (b(r) - A(r, [1:r-1,r+1:n])*x0([1:r-1,r+1:n]))/A(r,r);
         endfor
+        dx = abs(x - x0);
+        err = norm(dx);
+        relerr = err/(norm(x)+eps);
+        x0 = x;
+        if ((err < tol) || (relerr < tol)) break; endif
+    endfor
+endfunction
+
+function [x dx k] = jacobi2(A,b,x0,tol,maxit)
+    % Mètode iteratiu de Jacobi per resoldre Ax=b
+    % [IN] A matriu amb diagonal != 0    nxn
+    % [IN] b vector terme independent    nx1
+    % [IN] x0 primera aproximació        nx1
+    % [IN] tol tolerància en la solució  real
+    % [IN] maxit màxim d'iteraciosn      enter
+    % [OUT] x solució del sistema        nx1
+    % [OUT] dx mesura de l'error         real
+    % [OUT] k iteració on s'ha parat  enter
+    % condició radi espectral de (D\(A-D)) < 1
+    n = length(b);
+    B = - diag(1 ./ diag(A)) * (A-diag(diag(A)));
+    c = - diag(1 ./ diag(A)) * b;
+    x = x0;
+    for k = 1:maxit
+        x = B*x0 + c;
         dx = abs(x - x0);
         err = norm(dx);
         relerr = err/(norm(x)+eps);
